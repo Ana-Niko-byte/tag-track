de45ebbf9cad679847f867d564d7b0b51baae82b
Ana-Niko-byte <ananikolayenia@gmail.com>
2024-01-09 17:40:07 +0100

    Set prelim ASCII art heading

--- run.py
+++ run.py
@@ -1,2 +1,19 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from art import *
+from colorama import Fore
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+CREDS_SCOPE = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(CREDS_SCOPE)
+SHEET = GSPREAD_CLIENT.open('Tag-Track')
+
+text = text2art('Tag - Tracker')
+print(Fore.GREEN + 'Welcome to Your')
+print(Fore.GREEN + text + Fore.RESET)
