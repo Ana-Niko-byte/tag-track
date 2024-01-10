3747be1fba3019c58fc23cd400f6b6aaedf25f4c
Ana-Niko-byte <ananikolayenia@gmail.com>
2024-01-10 18:00:59 +0100

    Add prelim name input validation

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 from art import *
 from colorama import Fore
+import re
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -14,6 +15,28 @@ CREDS_SCOPE = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(CREDS_SCOPE)
 SHEET = GSPREAD_CLIENT.open('Tag-Track')
 
-text = text2art('Tag - Tracker')
-print(Fore.GREEN + 'Welcome to Your')
-print(Fore.GREEN + text + Fore.RESET)
+def print_intro():
+    text = text2art('Tag - Tracker')
+    print(Fore.GREEN + 'Welcome to')
+    print(Fore.GREEN + text + Fore.RESET)
+
+def validate_string(string):
+    return string.isalpha()
+
+def ask_name():
+    while True:
+        name = input('Please tell me your name: ')
+        if validate_string(name):
+            ask_month()
+            break
+
+    return name
+
+def ask_month():
+    print('month')
+    pass
+
+def main():
+    print_intro()
+    ask_name()
+main()
